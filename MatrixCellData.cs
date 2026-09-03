1a65740e7215deda78674e646d4e8b3178a02b7b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-03 21:14:20 +0900

    空のクラスと構造体を新規作成

---
+++ MatrixCellData.cs
@@ -0,0 +1,34 @@
+﻿//  -*-  coding: utf-8-with-signature  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  WpfControl.Editor  {
+
+//========================================================================
+//
+//    MatrixCellData  struct
+//
+/**
+**    行列のセルを管理する構造体。
+**
+**    メモリ効率を最大化するため値型（構造体）として定義。
+**/
+
+public  struct  MatrixCellData
+{
+
+
+}   //  End class  MatrixCellData
+
+}   //  End of namespace  WpfControl.Editor
