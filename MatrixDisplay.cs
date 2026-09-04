ba0b827d7824d6d00b3c52c14d8e4019df705483
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-04 12:57:20 +0900

    コンパイルエラーを修正

--- MatrixDisplay.cs
+++ MatrixDisplay.cs
@@ -106,8 +106,8 @@ OnRender(System.Windows.Media.DrawingContext  dc)
 
     Pen gridPen = new Pen(Brushes.LightGray, 0.5);
     Typeface typeface = new Typeface(
-            this.FontFamily, this.FontStyle, this.FontWeight,
-            this.FontStretch);
+            SystemFonts.CaptionFontFamily,
+            FontStyles.Normal,  FontWeights.Normal, FontStretches.Normal);
     double fontSize = Math.Min(cellHeight * 0.5, 12);
 
     dc.DrawRectangle(
