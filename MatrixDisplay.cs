9378b7a5cb2f74699f6017f3c607514d0125853c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-08 12:48:42 +0900

    コンパイルエラーを修正
    (1) プロジェクトの名前空間のタイプミス

--- MatrixDisplay.cs
+++ MatrixDisplay.cs
@@ -20,7 +20,7 @@ using   System.Windows.Media;
 using   WpfHelper.Controls;
 
 
-namespace  WpfControls.Editor  {
+namespace  WpfControl.Editor  {
 
 //========================================================================
 //
