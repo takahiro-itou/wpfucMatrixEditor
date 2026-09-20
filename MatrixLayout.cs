580a93005b3b163dc8ed6907d86f875b05d3850e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-20 12:56:54 +0900

    空のクラスを新規作成

---
+++ MatrixLayout.cs
@@ -0,0 +1,29 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  WpfControl.Editor  {
+
+//========================================================================
+//
+//    MatrixLayout  class
+//
+
+public  clas  MatrixLayout
+{
+
+
+}   //  End op class  MatrixLayout
+
+}   //  End of namespace  WpfControl.Editor
---
+++ MatrixOptions.cs
@@ -0,0 +1,29 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  WpfControl.Editor  {
+
+//========================================================================
+//
+//    MatrixOptions  class
+//
+
+public  clas  MatrixOptions
+{
+
+
+}   //  End op class  MatrixOptions
+
+}   //  End of namespace  WpfControl.Editor
